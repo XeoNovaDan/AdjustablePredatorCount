7426e46135e6b728cd1f3a8f75dae17107946c00
XeoNovaDan <toghill7252010@hotmail.co.uk>
2019-07-30 16:47:57 +0100

    Reworked

---
+++ Source/AdjustablePredatorCount/AdjustablePredatorCount/AdjustablePredatorCount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Harmony;
+using UnityEngine;
+
+namespace AdjustablePredatorCount
+{
+
+    public class AdjustablePredatorCount : Mod
+    {
+        public static AdjustablePredatorCountSettings settings;
+
+        public AdjustablePredatorCount(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<AdjustablePredatorCountSettings>();
+            HarmonyInstance = HarmonyInstance.Create("XeoNovaDan.AdjustablePredatorCount");
+        }
+
+        public override string SettingsCategory() => "AdjustablePredatorCount.SettingsCategory".Translate();
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            settings.DoWindowContents(inRect);
+        }
+
+        public static HarmonyInstance HarmonyInstance;
+
+    }
+}
